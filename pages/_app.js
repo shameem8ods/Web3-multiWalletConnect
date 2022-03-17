8d6820e8880067820e0dfb259c744a84cc1ae5c4
shameem <shameem@shameems-MacBook-Pro.local>
2022-03-17 17:58:28 +0530

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers";
+import { ChakraProvider } from '@chakra-ui/react'
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider);
+  return library;
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
+    </ChakraProvider>  
+  )  
 }
 
 export default MyApp
+
+
